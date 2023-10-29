4df1e14794e204ae835322c77841ab99b1915a3c
Jason Hamm <jasonhamm.me@gmail.com>
2023-10-29 12:53:54 -0600

    Use types in filters

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import TransactionCard from "../components/TransactionCard";
 import { Filters } from "../components/filters";
+import { State } from "../components/filters/state";
 
 const Home: NextPage = () => {
   const [transactions, setTransactions] = useState<any[]>([]);
+  const [filters, setFilters] = useState<State>();
   const fetchTransactions = async () => {
     const response = await fetch("/api/transactions");
     const data = await response.json();
@@ -19,7 +21,7 @@ const Home: NextPage = () => {
     <div className="w-full h-full">
       <div className="m-10">
         <h1 className="text-3xl font-semibold mb-6">Transactions</h1>
-        <Filters />
+        <Filters onChange={(values: State) => setFilters(values)} />
         <div className="flex flex-col gap-8 mt-10">
           {transactions &&
             transactions.map((transaction: any, index: number) => (
